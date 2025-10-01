885f91661a8bb5d4adfef1b434a74bdce3963fea
vatsark9 <ashumdsa@gmail.com>
2025-10-01 09:09:05 +0530

    Add styled header to app #20

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,11 @@ const generateSummary = () => {
     <div className="min-h-screen bg-gray-100 p-5 font-mono">
 
 
-      <h1>plumdejour</h1>
-      <p>Daily log tracker</p>
+<header className="mb-6 text-center">
+  <h1 className="text-4xl font-bold text-purple-600">plumdejour</h1>
+  <p className="text-lg text-gray-600">Your Daily Log Tracker</p>
+</header>
+
 
 <input
   type="text"
